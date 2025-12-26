9cad3cc3a557dc3d39df64d508a5b58bb2f29fa5
Dinann <notxurd46@gmail.com>
2025-12-26 20:53:17 +0700

    chore(deps): upgrade prisma to 7.2.0
    chore(deps): use pnpm-lock.yaml instead of package-lock.json

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,11 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../prisma/generated/prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const prisma = new PrismaClient({
+  adapter: adapter,
+});
