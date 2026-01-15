13a4665314bbb890e6d33c88dea929c35cb3e0d4
Dinann <notxurd46@gmail.com>
2026-01-15 21:27:58 +0700

    fix(token): specify algorithm for signing and verifying tokens

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -14,11 +14,11 @@ export const generateToken = async (payload: CustomPayload) => {
     sub: payload.id,
     exp: Math.floor(Date.now() / 1000) + 1 * 60 * 60, // Expires in 1hr
   };
-  return await sign(data, env.JWT_SECRET);
+  return await sign(data, env.JWT_SECRET, "HS256");
 };
 
 export const verifyToken = async (token: string) => {
-  return (await verify(token, env.JWT_SECRET)) as CustomPayload;
+  return (await verify(token, env.JWT_SECRET, "HS256")) as CustomPayload;
 };
 
 export const generateResetToken = async (payload: CustomPayload) => {
@@ -31,5 +31,5 @@ export const generateResetToken = async (payload: CustomPayload) => {
 };
 
 export const verifyResetToken = async (token: string) => {
-  return (await verify(token, env.JWT_RESET_SECRET)) as CustomPayload;
+  return (await verify(token, env.JWT_RESET_SECRET, "HS256")) as CustomPayload;
 };
